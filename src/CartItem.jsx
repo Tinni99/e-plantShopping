a2e736eb3571dbcce67e07bce366f57907c4a78f
Rhitaza Jana <janarhitaza@gmail.com>
2024-11-11 07:30:39 -0500

    CartItems component added

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,39 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return cart.reduce((total, item) => total + item.cost* item_quantity, 0);
   };
 
   const handleContinueShopping = (e) => {
-   
+   e.preventDefault();
+   onContinueShopping();
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  }
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({
+        ...item, quantity: item.quantity + 1}));
   };
 
   const handleDecrement = (item) => {
-   
+   if (item_quantity > 1) {
+    dispatch(updateQuantity({
+        ...item, quantity: item_quantity - 1}));
+   } else {
+    dispatch(removeItem(item));
+   }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.cost * item_quantity;
   };
 
   return (
